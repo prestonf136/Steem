f422340d708d13aebbccd9de93692d56f4618105
preston <prestonfebis100@gmail.com>
2021-02-17 06:42:58 -0500

    removed build folder

--- build/STEEM/src/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/preston/Documents/Steem/STEEM/src/tests
-# Build directory: /home/preston/Documents/Steem/build/STEEM/src/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(STEEM_TESTS "WindowTest")
-set_tests_properties(STEEM_TESTS PROPERTIES  _BACKTRACE_TRIPLES "/home/preston/Documents/Steem/STEEM/src/tests/CMakeLists.txt;6;add_test;/home/preston/Documents/Steem/STEEM/src/tests/CMakeLists.txt;0;")
