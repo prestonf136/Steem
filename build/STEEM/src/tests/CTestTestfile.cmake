f71a26df2b9c3e9a2a25857e05c0f568a48c442f
preston <prestonfebis100@gmail.com>
2021-02-17 06:36:34 -0500

    Initialized Testing

---
+++ build/STEEM/src/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/preston/Documents/Steem/STEEM/src/tests
+# Build directory: /home/preston/Documents/Steem/build/STEEM/src/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(STEEM_TESTS "WindowTest")
+set_tests_properties(STEEM_TESTS PROPERTIES  _BACKTRACE_TRIPLES "/home/preston/Documents/Steem/STEEM/src/tests/CMakeLists.txt;2;add_test;/home/preston/Documents/Steem/STEEM/src/tests/CMakeLists.txt;0;")
