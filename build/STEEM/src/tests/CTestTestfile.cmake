86cb2337ba490423907ab6620cc408c380e3c5fb
preston <prestonfebis100@gmail.com>
2021-02-17 06:42:06 -0500

    add an if and include to tests

--- build/STEEM/src/tests/CTestTestfile.cmake
+++ build/STEEM/src/tests/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(STEEM_TESTS "WindowTest")
-set_tests_properties(STEEM_TESTS PROPERTIES  _BACKTRACE_TRIPLES "/home/preston/Documents/Steem/STEEM/src/tests/CMakeLists.txt;2;add_test;/home/preston/Documents/Steem/STEEM/src/tests/CMakeLists.txt;0;")
+set_tests_properties(STEEM_TESTS PROPERTIES  _BACKTRACE_TRIPLES "/home/preston/Documents/Steem/STEEM/src/tests/CMakeLists.txt;6;add_test;/home/preston/Documents/Steem/STEEM/src/tests/CMakeLists.txt;0;")
