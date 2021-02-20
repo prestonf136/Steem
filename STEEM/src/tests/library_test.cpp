72a2d86e2c2dc426b9dea6386b1560bb04528b4f
preston <prestonfebis100@gmail.com>
2021-02-20 07:53:33 -0500

    added wireframe toggle, and vertex colors aswell as starting to understand vertex attribs better

--- STEEM/src/tests/library_test.cpp
+++ STEEM/src/tests/library_test.cpp
@@ -9,6 +9,16 @@ void fb_callb(GLFWwindow* window, int width, int height)
   glViewport(0, 0, width, height);
 };
 
+bool toggle = true;
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
+    {
+      glPolygonMode(GL_FRONT_AND_BACK, toggle ? GL_FILL : GL_LINE );
+      toggle = !toggle;
+    }
+}
+
 void processInput(GLFWwindow *window)
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -25,16 +35,20 @@ int main()
   GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);
   ST_ASSERT(window != nullptr);
   glfwMakeContextCurrent(window);
-  
+  glfwSetKeyCallback(window, key_callback);
+
   ST_ASSERT(gladLoadGLLoader((GLADloadproc)glfwGetProcAddress));
   glfwSetFramebufferSizeCallback(window, fb_callb);
 
   const char *VertexSource = 
   "#version 330 core\n"
   "layout (location = 0) in vec4 aPos;\n"
+  "layout (location = 1) in vec4 in_col;\n"
+  "out vec4 color;\n"
   "void main()\n"
   "{\n"
   "   gl_Position = aPos;\n"
+  "   color = in_col;"
   "}\0";
 
   GLuint VertexShader;
@@ -49,9 +63,10 @@ int main()
   const char *FragSource = 
   "#version 330 core\n"
   "out vec4 FragColor;\n"
+  "in vec4 color;\n"
   "void main()\n"
   "{\n"
-  "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+  "   FragColor = color;\n"
   "}\0";
 
   GLuint FragShader;
@@ -76,10 +91,10 @@ int main()
   glDeleteShader(FragShader);
 
   GLfloat vertices[] = {
-       0.5f,  0.5f,
-       0.5f, -0.5f,
-      -0.5f, -0.5f,
-      -0.5f,  0.5f,
+       0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
+       0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+      -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+      -0.5f,  0.5f, 1.0f, 1.0f, 0.0f,
   };
 
   GLuint indices[] = {
@@ -101,11 +116,13 @@ int main()
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 2, GL_FLOAT, GL_TRUE, 2 * sizeof(GLfloat), (void*)0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)0);
   glEnableVertexAttribArray(0);
-  
+
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(2 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
   glBindVertexArray(0);
-  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
   while(!glfwWindowShouldClose(window))
   {
     glfwSwapBuffers(window);
